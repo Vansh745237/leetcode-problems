b3adcb30546dffe431c7554ee4b4cd9e6d01621a
Vansh745237 <vanshbathla125@gmail.com>
2026-09-10 14:48:55 +0530

    Time: 3 ms (26.63%), Space: 9 MB (52.29%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const long long MOD = 1e9 + 7;
+        
+        vector<long long> dp(26, 0);
+
+        for (char c : s) {
+            int x = c - 'a';
+
+            long long total = 1; // empty subsequence
+
+            for (int i = 0; i < 26; i++) {
+                total = (total + dp[i]) % MOD;
+            }
+
+            dp[x] = total;
+        }
+
+        long long ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            ans = (ans + dp[i]) % MOD;
+        }
+
+        return ans;
+    }
+};
